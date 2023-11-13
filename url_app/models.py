2260ab5bb31f13779e526acd277d8273652aed1c
mcstao <arislg99@gmail.com>
2023-11-13 17:10:21 +0600

    Added save function

--- url_app/models.py
+++ url_app/models.py
@@ -6,4 +6,10 @@ class Url(models.Model):
     full_url = models.URLField(max_length=1000, verbose_name="Полная ссылка")
     short_url = models.CharField(max_length=30, unique=True, verbose_name="Короткая ссылка", blank=True)
 
+    def __str__(self):
+        return self.full_url, self.short_url
 
+    def save(self, *args, **kwargs):
+        if not self.short_url:
+            self.short_url = str(uuid.uuid4())[:7]
+        super().save(*args, **kwargs)
